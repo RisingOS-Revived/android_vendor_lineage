c551971c5d5c2cfa57d50a301629db1770594228
Austin Young <austineyoung2000@gmail.com>
2026-09-01 19:28:21 -0400

    config: Removes old FaceUnlock configuration as its moved
    Eliminates the conditional inclusion of FaceUnlock-related packages from vendor/lineage , system properties, and permissions from the common build configuration. This streamlines the makefile by removing a deprecated or relocated feature setup.

--- config/common.mk
+++ config/common.mk
@@ -281,21 +281,6 @@ PRODUCT_ARTIFACT_PATH_REQUIREMENT_ALLOWED_LIST += \
     system/bin/setcap \
     system/%/libzstd.so
 
-ifeq ($(TARGET_SUPPORTS_64_BIT_APPS),true)
-ifeq ($(TARGET_SUPPORTS_GFU),true)
-$(call inherit-product-if-exists, vendor/google/faceunlock/config.mk)
-else
-PRODUCT_PACKAGES += \
-    FaceUnlock
-
-PRODUCT_SYSTEM_EXT_PROPERTIES += \
-    ro.face.sense_service=true
-
-PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.biometrics.face.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/android.hardware.biometrics.face.xml
-endif
-endif
-
 # fastbootd
 ifneq ($(TARGET_DISABLE_FASTBOOTD),true)
 PRODUCT_PACKAGES += \
